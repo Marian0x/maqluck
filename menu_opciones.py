cad7f7ee2eeb4dccbcb64acd36ef9f472255b8ee
Mariano <mmjm26@gmail.com>
2023-07-16 17:09:00 -0300

    se agrega función ingresa_opciones

--- menu_opciones.py
+++ menu_opciones.py
@@ -1,9 +1,22 @@
-class menu:
-    def __init__(self, *args, opcion):
-        self.opcion = opcion
+class Menu:
+    def __init__(self, *args):
         self.args = args
-
+    
     def mostrar(self):
-        for arg in self.args:
-            print
+        for i, arg in enumerate(self.args):
+            print(i+1,' - ',arg)
+        print(f"\n{0}", " - ", "Salir")
 
+def ingresa_opciones():
+    print("*** Ingresa opciones de tu menú *** ")
+    opcion = "S"
+    lista = []
+    while opcion.upper() == "S":
+        entrada_menu = input("Ingresa opción: ")
+        lista.append(entrada_menu)
+        opcion = input("¿Ingresar otra opción? (s/n): ")
+    return lista
+
+# Instancias ejemplos
+menu_lucky = Menu("Agregar", "Mostrar", "Buscar")
+menu_lucky.mostrar()
